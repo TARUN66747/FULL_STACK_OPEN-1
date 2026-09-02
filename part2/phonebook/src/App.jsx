524ba581284de78ea4525b5aaa7ae8c5018bef9e
TARUN <24amtics397@gmail.com>
2026-09-02 11:24:09 +0530

    2.12 : phonebook data wrote on json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -44,15 +44,21 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id : persons.length+1
+      // id : persons.length+1
     }
+    
+    
     if (isDuplicate(personObject.name)){
       alert(`${newName} is already added to phonebook`)
     }
     else{
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then((response) => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
