794055d0f6e135871b025e46f76eff289f644554
TARUN <24amtics397@gmail.com>
2026-09-06 12:21:58 +0530

    3.1 and 3.2 completed backend of phone

---
+++ part3/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app =express()
+
+const person = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(request,response)=>{
+    response.json(person)
+})
+app.get('/info',(request,response)=>{
+    const count = person.length
+    const date = new Date()
+
+    response.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${date}</p>`)
+
+    })
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
