349540f05220c6747952e64125b9cc4c38ddc38a
TARUN <24amtics397@gmail.com>
2026-09-08 00:32:26 +0530

    3.7 exercise for using morgan is completed

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app =express()
 app.use(express.json())
+
+app.use(morgan('tiny'))
 let person = [
     { 
       "id": "1",
@@ -25,8 +28,10 @@ let person = [
 ]
 
 app.get('/api/persons',(request,response)=>{
+  
     response.json(person)
 })
+
 app.get('/info',(request,response)=>{
     const count = person.length
     const date = new Date()
